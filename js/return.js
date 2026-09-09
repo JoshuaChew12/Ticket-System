aa71e98fb1e6df4a5f031b93bd8b776d08ecda5f
JoshuaChew12 <jayz.chew.one@gmail.com>
2026-09-09 18:20:48 +0800

    Add initReturn function for sales return management

---
+++ js/return.js
@@ -0,0 +1,11 @@
+function initReturn(){
+
+  const status=document.querySelector(
+    "#pageContent .status"
+  );
+
+  if(status){
+    status.textContent=
+      "Sales Return management will be available here.";
+  }
+}
